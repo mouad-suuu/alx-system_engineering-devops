866f2a85870c00074406db8c9f07defea044cbaa
mouad-suuu <moad.menniui@gmail.com>
2023-12-22 21:57:59 +0100

    fixing task 3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,19 @@
-# Ensure Python 3 is installed
-package { 'python3':
-  ensure => installed,
+# 1-install_a_package.pp
+
+# Install Python 3.8.10
+class { 'python':
+  version => '3.8.10',
 }
 
-# Ensure Pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask v2.1.0
+# Install Flask 2.1.0 and Werkzeug 2.1.1
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Class['python'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Class['python'],
 }
