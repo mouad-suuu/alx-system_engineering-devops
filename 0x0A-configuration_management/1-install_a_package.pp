24ab439917a2d5d6659c5a687e1735236d29aec3
mouad-suuu <moad.menniui@gmail.com>
2023-12-22 21:31:24 +0100

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# Installs Flask v2.1.0
-package { 'flask':
+# Puppet Manifest: 1-install_a_package.pp
+# Description: Install Flask version 2.1.0 using pip3
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
