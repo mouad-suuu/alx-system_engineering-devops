e0ddd53580eb578c26982026bc9d772bbc84e696
mouad-suuu <moad.menniui@gmail.com>
2023-12-22 21:34:00 +0100

    fixing task 2 for the 100 time

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-# Puppet Manifest: 1-install_a_package.pp
-# Description: Install Flask version 2.1.0 using pip3
+# Ensure Python 3 is installed
+package { 'python3':
+  ensure => installed,
+}
 
+# Ensure Pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask v2.1.0
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
