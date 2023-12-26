ece56579830331b54ea8576346da5ef3654ecec6
mouad-suuu <moad.menniui@gmail.com>
2023-12-25 20:05:12 -0800

    client file configuration with puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# 100-puppet_ssh_config.pp
+
+# Ensure SSH client is configured to use the private key ~/.ssh/school
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+}
+
+# Ensure SSH client is configured to refuse password authentication
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+}
+
