ba1313d0c756b3f43c04e8d5ae0229266ee12717
mouad-suuu <moad.menniui@gmail.com>
2024-02-13 20:33:26 +0100

    task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 """
-whatever comment
+Query Reddit API for number of subscribers for a given subreddit
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-
+    """
+        return number of subscribers for a given subreddit
+        return 0 if invalid subreddit given
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
+    # get user agent
+    # https://stackoverflow.com/questions/10606133/ -->
+    # sending-user-agent-using-requests-library-in-python
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
 
